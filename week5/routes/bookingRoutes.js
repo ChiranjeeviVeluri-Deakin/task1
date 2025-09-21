09724c7e389142053f705c6fbb17305d12e5d3ab
ChiranjeeviVeluri-Deakin <s224677292@deakin.edu.au>
2025-09-21 06:52:39 +0000

    week7

--- week5/routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const c = require('../controllers/bookingController');
-
-router.post('/', c.create); // POST /api/bookings
-router.get('/', c.list);    // GET  /api/bookings
-
-module.exports = router;
